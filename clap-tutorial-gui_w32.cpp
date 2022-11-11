a30571b00fbec9ae4476e75b51f84cca725464a5
Nakst <17091026+nakst@users.noreply.github.com>
2022-11-11 16:30:38 -0500

    Add files via upload

--- clap-tutorial-gui_w32.cpp
+++ clap-tutorial-gui_w32.cpp
@@ -9,6 +9,11 @@ struct GUI {
 
 static int globalOpenGUICount = 0;
 
+static void GUIPaint(MyPlugin *plugin, bool internal) {
+	if (internal) PluginPaint(plugin, plugin->gui->bits);
+	RedrawWindow(plugin->gui->window, 0, 0, RDW_INVALIDATE);
+}
+
 LRESULT CALLBACK GUIWindowProcedure(HWND window, UINT message, WPARAM wParam, LPARAM lParam) {
 	MyPlugin *plugin = (MyPlugin *) GetWindowLongPtr(window, 0);
 
@@ -19,15 +24,20 @@ LRESULT CALLBACK GUIWindowProcedure(HWND window, UINT message, WPARAM wParam, LP
 	if (message == WM_PAINT) {
 		PAINTSTRUCT paint;
 		HDC dc = BeginPaint(window, &paint);
-		BITMAPINFO info = { { sizeof(BITMAPINFOHEADER), GUI_WIDTH, GUI_HEIGHT, 1, 32 } };
+		BITMAPINFO info = { { sizeof(BITMAPINFOHEADER), GUI_WIDTH, -GUI_HEIGHT, 1, 32, BI_RGB } };
 		StretchDIBits(dc, 0, 0, GUI_WIDTH, GUI_HEIGHT, 0, 0, GUI_WIDTH, GUI_HEIGHT, plugin->gui->bits, &info, DIB_RGB_COLORS, SRCCOPY);
 		EndPaint(window, &paint);
 	} else if (message == WM_MOUSEMOVE) {
 		PluginProcessMouseDrag(plugin, GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam));
+		GUIPaint(plugin, true);
 	} else if (message == WM_LBUTTONDOWN) {
+		SetCapture(window); 
 		PluginProcessMousePress(plugin, GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam));
+		GUIPaint(plugin, true);
 	} else if (message == WM_LBUTTONUP) {
+		ReleaseCapture(); 
 		PluginProcessMouseRelease(plugin);
+		GUIPaint(plugin, true);
 	} else {
 		return DefWindowProc(window, message, wParam, lParam);
 	}
@@ -53,6 +63,8 @@ static void GUICreate(MyPlugin *plugin) {
 			CW_USEDEFAULT, 0, GUI_WIDTH, GUI_HEIGHT, GetDesktopWindow(), NULL, NULL, NULL);
 	plugin->gui->bits = (uint32_t *) calloc(1, GUI_WIDTH * GUI_HEIGHT * 4);
 	SetWindowLongPtr(plugin->gui->window, 0, (LONG_PTR) plugin);
+
+	PluginPaint(plugin, plugin->gui->bits);
 }
 
 static void GUIDestroy(MyPlugin *plugin) {
@@ -70,8 +82,3 @@ static void GUIDestroy(MyPlugin *plugin) {
 #define GUISetParent(plugin, parent) SetParent((plugin)->gui->window, (HWND) (parent)->win32)
 #define GUISetVisible(plugin, visible) ShowWindow((plugin)->gui->window, (visible) ? SW_SHOW : SW_HIDE)
 static void GUIOnPOSIXFD(MyPlugin *) {}
-
-static void GUIPaint(MyPlugin *plugin, bool internal) {
-	if (internal) PluginPaint(plugin, plugin->gui->bits);
-	RedrawWindow(plugin->gui->window, 0, 0, RDW_INVALIDATE);
-}
